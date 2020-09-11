807c3f6cc499168675658d1388824789a0acbfbc
Oliver Taylor <olivertaylor@users.noreply.github.com>
2020-09-10 22:29:56 -0700

    Update main.go

--- main.go
+++ main.go
@@ -43,15 +43,15 @@ var (
 		"https://www.raptitude.com/feed/",
 		//"http://www.aaronsw.com/2002/feeds/pgessays.rss"
 		// emacs feeds
-		"https://oremacs.com/atom.xml",
-		"https://irreal.org/blog/?feed=rss2",
-		"https://endlessparentheses.com/atom.xml",
-		"https://200ok.ch/atom.xml",
-		"https://with-emacs.com/rss.xml",
-		"https://nullprogram.com/tags/emacs/feed/",
-		"https://sachachua.com/blog/category/emacs-news/feed",
-		"https://protesilaos.com/codelog.xml",
-		"https://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-only.xml",
+		//"https://oremacs.com/atom.xml",
+		//"https://irreal.org/blog/?feed=rss2",
+		//"https://endlessparentheses.com/atom.xml",
+		//"https://200ok.ch/atom.xml",
+		//"https://with-emacs.com/rss.xml",
+		//"https://nullprogram.com/tags/emacs/feed/",
+		//"https://sachachua.com/blog/category/emacs-news/feed",
+		//"https://protesilaos.com/codelog.xml",
+		//"https://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-only.xml",
 	}
 
 	// Show up to 60 days of posts
